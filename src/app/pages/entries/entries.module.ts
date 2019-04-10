52a072bd5e42eaf1bdde7589ec974048146ec3ca
Isaac Burmann <isaac.burmann@gmail.com>
2019-04-10 15:33:19 -0300

    project restructuring with shared modules

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { EntriesRoutingModule } from './entries-routing.module';
-import {EntryListComponent} from './entry-list/entry-list.component';
-import {EntryFormComponent} from './entry-form/entry-form.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {CalendarModule} from 'primeng/primeng';
-import {IMaskModule} from 'angular-imask';
+import { EntryListComponent } from './entry-list/entry-list.component';
+import { EntryFormComponent } from './entry-form/entry-form.component';
+import { SharedModule } from '../../shared/shared.module';
+import { CalendarModule } from 'primeng/primeng';
+import { IMaskModule } from 'angular-imask';
 
 @NgModule({
   declarations: [EntryListComponent, EntryFormComponent],
   imports: [
-    CommonModule,
+    SharedModule,
     EntriesRoutingModule,
-    ReactiveFormsModule,
     CalendarModule,
     IMaskModule
   ]
