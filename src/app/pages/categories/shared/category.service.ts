d935069286bcc7f2e75308db56ff5ef8cedf207a
Isaac Burmann <isaac.burmann@gmail.com>
2019-04-10 18:57:59 -0300

    adding BaseResourceService

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Observable, throwError} from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
-import {CategoryModel} from './category.model';
+import { CategoryModel } from './category.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,63 +9,4 @@ export class CategoryService {
   private apiPath = 'api/categories';
 
   constructor(private http: HttpClient) { }
-
-  getAll(): Observable<CategoryModel[]> {
-    return this.http.get(this.apiPath).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToCategories)
-    );
-  }
-
-  getById(id: number): Observable<CategoryModel> {
-    const url = `${this.apiPath}/${id}`;
-
-    return this.http.get(url).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToCategory)
-    );
-  }
-
-  create(category: CategoryModel): Observable<CategoryModel> {
-    return this.http.post(this.apiPath, category).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToCategory)
-    );
-  }
-
-  update(category: CategoryModel): Observable<CategoryModel> {
-    const url = `${this.apiPath}/${category.id}`;
-
-    return this.http.put(url, category).pipe(
-      catchError(this.handleError),
-      map(() => category)
-    );
-  }
-
-  delete(id: number): Observable<any> {
-    const url = `${this.apiPath}/${id}`;
-
-    return this.http.delete(url).pipe(
-      catchError(this.handleError),
-      map(() => null)
-    );
-  }
-
-  // PRIVATE METHODS
-  private jsonDataToCategories(jsonData: any[]): CategoryModel[] {
-    const categories: CategoryModel[] = [];
-    jsonData.forEach(item => categories.push(item as CategoryModel));
-
-    return categories;
-  }
-
-  private jsonDataToCategory(jsonData: any): CategoryModel {
-    return jsonData as CategoryModel;
-  }
-
-  private handleError(error: any): Observable<any> {
-    console.log('ERRO NA REQUISICAO => ', error);
-
-    return throwError(error);
-  }
 }
