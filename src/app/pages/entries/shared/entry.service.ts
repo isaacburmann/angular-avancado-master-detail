11be0772af08e15d929ad7f8a265db44cc255310
Isaac Burmann <isaac.burmann@gmail.com>
2019-04-15 22:33:56 -0300

    improving reports component

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, Injector } from '@angular/core';
 import { EntryModel } from './entry.model';
 import { BaseResourceService } from '../../../shared/services/base-resource.service';
-import { catchError, flatMap } from 'rxjs/internal/operators';
+import { catchError, flatMap, map } from 'rxjs/internal/operators';
 import { Observable } from 'rxjs';
 import { CategoryService } from '../../categories/shared/category.service';
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +23,12 @@ export class EntryService extends BaseResourceService<EntryModel> {
     return this.setCategoryAndSendToServer(entry, super.update.bind(this));
   }
 
+  getByMonthAndYear(month: number, year: number): Observable<EntryModel[]> {
+    return this.getAll().pipe(
+      map(entries => this.filterByMonthAndYear(entries, month, year))
+    );
+  }
+
   private setCategoryAndSendToServer(entry: EntryModel, sendFn: any): Observable<EntryModel> {
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap(category => {
@@ -30,5 +37,17 @@ export class EntryService extends BaseResourceService<EntryModel> {
       }),
       catchError(this.handleError)
     );
+  }
+
+  private filterByMonthAndYear(entries: EntryModel[], month: number, year: number) {
+    return entries.filter(entry => {
+      const entryDate = moment(entry.date, 'DD/MM/YYYY');
+      const monthMatches = entryDate.month() + 1 === month;
+      const yearMatches = entryDate.year() === year;
+
+      if (monthMatches && yearMatches) {
+        return entry;
+      }
+    });
   }
 }
--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CategoryService } from '../../categories/shared/category.service';
+import { EntryService } from '../../entries/shared/entry.service';
+import { CategoryModel } from '../../categories/shared/category.model';
+import { EntryModel } from '../../entries/shared/entry.model';
 
 @Component({
   selector: 'app-reports',
@@ -7,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  expenseTotal: any = 0;
+  revenueTotal: any = 0;
+  balance: any = 0;
+
+  expenseChartData: any;
+  revenueChartData: any;
+
+  chartOptions = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+          }
+      }]
+    }
+  };
+
+  categories: CategoryModel[] = [];
+  entries: EntryModel[] = [];
+
+  @ViewChild('month') month: ElementRef = null;
+  @ViewChild('year') year: ElementRef = null;
+
+  constructor(
+    private categoryService: CategoryService,
+    private entryService: EntryService
+  ) { }
 
   ngOnInit() {
+    this.categoryService.getAll()
+      .subscribe(categories => this.categories = categories);
+  }
+
+  generateReports() {
+    const month = this.month.nativeElement.value;
+    const year = this.year.nativeElement.value;
+
+    if (!month || !year) {
+      alert('Selecione o mes e o ano para gerar o relatorio');
+    } else {
+      this.entryService.getByMonthAndYear(month, year);
+    }
   }
 
 }
